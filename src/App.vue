921f106fb00bdb834cf0a8167ec46563de79ba25
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-12-09 23:42:13 +0800

    hero and card

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,11 @@
 <script setup>
-import { ref } from "vue";
-const name = ref("jhon doe");
-const status = ref("active");
-const tasks = ref(["task one", "task two", "task three"]);
-
-
-const toggleStatus = () => {
-    if (status.value === "active") {
-        status.value = "pending";
-    } else if (status.value === "pending") {
-        status.value = "inactive";
-    } else {
-        status.value = "active";
-    }
-};
-const addTask = () => {
-    if(newTask.value.trim() !== ''){
-        tasks.value.push(newTask.value);
-        newTask.value = '';
-    }
-};
-
+import Navbar from './components/navbar.vue';
+import Hero from './components/hero.vue';
+import HomeCard from './components/homecard.vue';
 </script>
 
 <template>
-    <h1>{{ name }}</h1>
-    <p v-if="status === 'active'">User Is Active</p>
-    <p v-else-if="status === 'pending'">User Pending</p>
-    <p v-else-if="status">User is Inactive</p>
-
-	<form @submit.prevent="addTask">
-		<label for="newTask">Add Task</label>
-		<input type="text" name="newTask" id="newTask" v-model="newTask">
-		<button type="submit">Add Task</button>
-	</form>
-
-    <h3>Task</h3>
-    <ul>
-        <li v-for="task in tasks" :key="task">{{ task }}</li>
-    </ul>
-    <!-- <button v-on:click="togglestatus">Change Status</button> -->
-    <button @click="toggleStatus">Change Status</button>
+	<Navbar />
+	<Hero />
+	<HomeCard />
 </template>
--- src/App2.vue
+++ src/App2.vue
@@ -1,25 +1,39 @@
-<script>
-export default {
-    data() {
-        return {
-            name: "jhon doe",
-            status: "active",
-            tasks: ["task one", "task two", "task three"],
-            link: "https://www.google.com",
-        };
-    },
-	methods:{
-		togglestatus(){
-			if (this.status === 'active') {
-				this.status = 'pending';
-			} else if (this.status === 'pending') {
-				this.status = 'inactive';
-			} else {
-				this.status = 'active';
-			}
-		}
-	}
+<script setup>
+import { onMounted, ref } from "vue";
+const name = ref("jhon doe");
+const status = ref("active");
+const tasks = ref(["task one", "task two", "task three"]);
+
+const toggleStatus = () => {
+    if (status.value === "active") {
+        status.value = "pending";
+    } else if (status.value === "pending") {
+        status.value = "inactive";
+    } else {
+        status.value = "active";
+    }
 };
+const addTask = () => {
+    if (newTask.value === "") {
+        alert("Please enter a task");
+    } else {
+        tasks.value.push(newTask.value);
+    }
+};
+
+const deteleTask = (index) => {
+    tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+	try {
+		const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+		const data = await response.json();
+		tasks.value = data.map((task) => task.title);	
+	} catch (error) {
+		console.log(error);
+	}
+});
 </script>
 
 <template>
@@ -28,13 +42,21 @@ export default {
     <p v-else-if="status === 'pending'">User Pending</p>
     <p v-else-if="status">User is Inactive</p>
 
+    <form @submit.prevent="addTask">
+        <label for="newTask">Add Task</label>
+        <input type="text" name="newTask" id="newTask" v-model="newTask" />
+        <button type="submit">Add Task</button>
+    </form>
+
     <h3>Task</h3>
     <ul>
-        <li v-for="task in tasks" :key="task">{{ task }}</li>
+        <li v-for="(task, index) in tasks" :key="task">
+            <span>
+                {{ task }}
+            </span>
+            <button @click="deteleTask">X</button>
+        </li>
     </ul>
-
-    <!-- <a v-bind:href="link">Click for Google</a> -->
-	<a :href="link">Click for Google</a>
-	<!-- <button v-on:click="togglestatus">Change Status</button> -->
-	<button @click="togglestatus">Change Status</button>
+    <!-- <button v-on:click="togglestatus">Change Status</button> -->
+    <button @click="toggleStatus">Change Status</button>
 </template>
