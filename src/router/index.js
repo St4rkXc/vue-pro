e42d9666930f16f5a60699b8f7e1416d00af92b6
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-12-17 23:48:47 +0800

    not vound view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import homeView from "@/views/homeView.vue";
 import jobView from "@/views/JobsView.vue";
+import NotFoundView from "@/views/notFoundView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
             path: "/jobs",
             name: "jobs",
             component: jobView,
+        },
+        {
+            path: "/:catchAll(.*)",
+            name: "not-found",
+            component: NotFoundView,
         }
     ],
 });
---
+++ src/views/notFoundView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+</script>
+
+<template>
+    <section class="flex flex-col items-center justify-center text-center h-96">
+        <i class="mb-4 text-yellow-400 fas fa-exclamation-triangle fa-4x"></i>
+        <h1 class="mb-4 text-6xl font-bold">404 Not Found</h1>
+        <p class="mb-5 text-xl">This page does not exist</p>
+        <RouterLink to="/" class="px-3 py-2 mt-4 text-white bg-green-700 rounded-md hover:bg-green-900">Go Back</RouterLink>
+    </section>
+</template>
